a1489c080dc751581c7c46c0823d2dc77117e96c
Danilo Mendonça <danilomendonca08@gmail.com>
2024-04-17 20:51:46 -0300

    create register view and RegisterDTO

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CHECKPOINT2_DOTNET.Data;
+using CHECKPOINT2_DOTNET.DTOs;
+using CHECKPOINT2_DOTNET.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CHECKPOINT2_DOTNET.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ILogger<UserController> _logger;
+        private readonly DataContext _dataContext;
+
+        public UserController(ILogger<UserController> logger, DataContext dataContext)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Register()
+        public IActionResult Register(RegisterDTO request)
         {
+            var user = _dataContext.UserFiap.FirstOrDefault(x => x.UserEmail == request.UserEmail);
+            if (user != null)
+            {
+                return BadRequest("Usuário ja existe");
+            }
+            User NewUser = new User
+            {
+                UserEmail = request.UserEmail,
+                UserName = request.UserName,
+                UserPassword = request.UserPassword,
+                UserPhone = request.UserPhone,
+            };
+            _dataContext.Add(NewUser);
+            _dataContext.SaveChanges();
             return View();
         }
     }
---
+++ DTOs/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CHECKPOINT2_DOTNET.DTOs
+{
+
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; } = string.Empty;
+        [DataType(DataType.Password)]
+        [Compare("UserPassword", ErrorMessage = "As Senhas não são iguais!!")]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string UserPhone { get; set; } = string.Empty;
+    }
+}
