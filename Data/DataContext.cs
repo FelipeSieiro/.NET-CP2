ea622642de1149cb57e89bf601eff210d6160ddf
FelipeSieiro <felipedossantos0804@gmail.com>
2024-04-17 20:10:45 -0300

    implements database oracle in project

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using CHECKPOINT2_DOTNET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CHECKPOINT2_DOTNET.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> UserFiap { get; set; }
+    }
+}
---
+++ Migrations/20240417231004_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CHECKPOINT2_DOTNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFiap",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    UserName = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserEmail = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserPassword = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserPhone = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFiap", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFiap");
+        }
+    }
+}
