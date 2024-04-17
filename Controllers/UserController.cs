569b7235e2c90b5bc2814a1ddbcb0f7269e00a12
codelize <lflizier@gmail.com>
2024-04-17 20:33:49 -0300

    create user controller and view

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CHECKPOINT2_DOTNET.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
