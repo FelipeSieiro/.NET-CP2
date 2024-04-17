e4bf65022bb2921fd0af04b5df6ad196419042a9
FelipeSieiro <felipedossantos0804@gmail.com>
2024-04-17 20:00:43 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CHECKPOINT2_DOTNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
